145829a7a4937884ccc5853036a823d6e54539c8
DanielFelipeFernandezCAMPUS <github.frnandz@gmail.com>
2023-02-23 07:41:11 -0500

    Punto_9

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-""" 8. Escriba un bloque cualquiera de código en Python en donde
-utilice 2 condicionales (if) anidados. """
 
-numero_uno = int(input("Ingresa el primer numero: "))
-numero_dos = int(input("Ingresa el segundo numero :"))
+""" 9. Construya un algoritmo en Python, que permita ingresar la
+información de un empleado e imprima el nombre, los
+apellidos y la antigüedad. Los datos que se deben solicitar
+son los siguientes:
+*Nombre * Teléfono *Año de ingreso a la empresa
+*Apellidos *Edad. """
 
+actual = 2023
+nombre = input("Ingresa el nombre del empleado: ")
+apellidos = input("Ingresa los apellidos del empleado: ")
+telefono = input("Ingresa el telefono del empleado: ")
+edad = int(input("Ingresa la edad del empleado: "))
+ingreso = int(input("Ingresa el año de ingreso del empleado a la empresa:"))
 
-if (numero_uno == numero_dos):       
-   print("Los numeros son iguales")    
+antiguedad = actual - ingreso
+
+if (antiguedad<0 or edad <0):
+    print("Fecha de ingreso no puede ser mayor al año actual")
 else:
-    if(numero_uno>numero_dos):
-      print("El primero es mayor")
-    else:
-       print("El segundo es mayor") 
+    print(f"\tDatos del Empleado: \nNombre: {nombre} \nApellidos: {apellidos}")
+    print(f"Telefono: {telefono} \nEdad: {edad}")
+    print(f"La antigüedad del empleado es: {antiguedad} años")
