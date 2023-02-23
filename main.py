e357105a9c3635c57257a8b8ca108e9793160e3b
DanielFelipeFernandezCAMPUS <github.frnandz@gmail.com>
2023-02-23 07:40:23 -0500

    Punto_7

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 
+""" 7. Cuál es la diferencia entre un condicional simple y un
+condicional compuesto? """
 
-""" 6. En sus propias palabras, qué son las funciones
-preconstruidas y proporcione 2 ejemplos. """
-
-print("\nSon funciones que ya vienen en los paquetes del lenguaje de programación " 
-      "Python, solo debemos instanciarlas y tener en cuenta la finalidad de esa función preconstruida o predefinida")
-
-print("\t1.Función sort\n\nUna de las funciones mas usadas en Python es .sort(), su funcionalidad es "
-      "ordenar de manera ascendente o descendente una lista ya creada.")
-
-print("Creamos una lista carros \ncarros = ['Ferrari', 'Lamborghini', 'Bugatti', 'Volvo']")
-
-carros = ['Ferrari', 'Lamborghini', 'Bugatti', 'Volvo']
-carros.sort()
-
-print("Aplicarle sort de esta manera carros.sort(), nos ordenará la lista de la siguiente manera: \n", carros)
-
-print("\n\t2. Función min\n\nRetorna la unidad minima dentro de una lista," 
-      " usemos la lista ya creada de carros, siendo esta lista de cadenas "
-      " de caracteres tomará la unidad minima en ordena alfabetico", min(carros))
+print("La principal diferencia que existe entre este tipo de condicionales "
+      " es que el Condicional SIMPLE solo se deriva una ruta de la condicion (si), a diferencia del condicional "
+      "compuesto, existen dos rutas de salida del condicion (si/no)")
