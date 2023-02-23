da09dfe758f915d9ce273d334f349d336224a357
DanielFelipeFernandezCAMPUS <github.frnandz@gmail.com>
2023-02-23 07:39:06 -0500

    Punto_3

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-""" 2. En la jerarquía de operadores, cuáles son los que más
-prioridad tienen cuando el intérprete de Python los evalúa? """
 
-print("\n\tJerarquía Python")
-print("1. Operaciones entre paréntesis ()")
-print("2. Potencia **")
-print("3. Multiplicación y División, módulo o residuo, División entera *, /, %, // ")
-print("4. Suma y resta + , -")
-print("5. Operadores relacionales <, <=, >, >=, !=, ==")
-print("6. Operador lógico AND And")
-print("7. Operador lógico OR Or")
+""" 3. Si hay operadores de igual precedencia, en qué orden se
+ejecutan?
+A. De izquierda a derecha
+B. De derecha a izquierda """
 
-print("Ejemplo: (((4**5))*(10+9-(78))+(6//4))+(8/4) \nResultado")
-print((((4**5))*(10+9-(78))+(6//4))+(8/4))
+print("A. De izquierda a derecha")
