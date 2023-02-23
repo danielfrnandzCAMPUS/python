11e2bb61013a051bb6c97233958b71f2b609934e
DanielFelipeFernandezCAMPUS <github.frnandz@gmail.com>
2023-02-23 07:39:23 -0500

    Punto_4

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+""" 4. Que son las expresiones regulares en Python? """
+print("Las expresiones resgulares son secuencias de caracteres que forma un patrón de búsqueda,"
+      " las cuales son formalizadas por medio de una sintaxis específica."
+      " Los patrones se interpretan como un conjunto de instrucciones,"
+      " que luego se ejecutan sobre un texto de entrada para producir"
+      " una versión modificada del texto original. ")
 
-""" 3. Si hay operadores de igual precedencia, en qué orden se
-ejecutan?
-A. De izquierda a derecha
-B. De derecha a izquierda """
 
-print("A. De izquierda a derecha")
+print("Info: https://relopezbriega.github.io/blog/2015/07/19/expresiones-regulares-con-python/")
